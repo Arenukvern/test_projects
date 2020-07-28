143cf517d4caf940939375ed640876166aa4eb5f
Anton Malofeev <arenukvern@gmail.com>
2020-07-29 00:56:25 +0300

    Add: project structure Add: rules for eslint, jest, prettier and vs code workspace

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,31 +1,43 @@
 module.exports = {
   root: true,
   env: {
-    node: true
+    node: true,
+    browser: true,
+    es6: true,
   },
   extends: [
-    "plugin:vue/essential",
-    "eslint:recommended",
-    "@vue/typescript/recommended",
-    "@vue/prettier",
-    "@vue/prettier/@typescript-eslint"
+    'plugin:vue/essential',
+    'eslint:recommended',
+    '@vue/typescript/recommended',
+    '@vue/prettier',
+    '@vue/prettier/@typescript-eslint',
   ],
-  parserOptions: {
-    ecmaVersion: 2020
-  },
   rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off"
+    'no-console': process.env.NODE_ENV === 'production' ? 'off' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'off' : 'off',
+    'no-case-declarations': 'off',
+    'no-inner-declarations': 'off',
+    'no-fallthrough': 'off',
+    'no-prototype-builtins': 'off',
+    'no-unused-vars': 'off',
+    indent: [2, 2, { SwitchCase: 1 }],
+    quotes: ['error', 'single'],
+    // we want to force semicolons
+    semi: ['error', 'never'],
+  },
+  parserOptions: {
+    ecmaVersion: 2020,
+    parser: '@typescript-eslint/parser',
   },
   overrides: [
     {
       files: [
-        "**/__tests__/*.{j,t}s?(x)",
-        "**/tests/unit/**/*.spec.{j,t}s?(x)"
+        '**/__tests__/*.{j,t}s?(x)',
+        '**/tests/unit/**/*.spec.{j,t}s?(x)',
       ],
       env: {
-        jest: true
-      }
-    }
-  ]
-};
+        jest: true,
+      },
+    },
+  ],
+}
--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,21 @@
 module.exports = {
-  preset: "@vue/cli-plugin-unit-jest/presets/typescript-and-babel"
-};
+  moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx', 'json', 'vue'],
+  preset: '@vue/cli-plugin-unit-jest/presets/typescript-and-babel',
+  setupFiles: ['./tests/unit/jest-setup.js'],
+  transform: {
+    '^.+\\.vue$': 'vue-jest',
+    '.+\\.(css|styl|less|sass|scss|svg|png|jpg|ttf|woff|woff2)$':
+      'jest-transform-stub',
+    '^.+\\.tsx?$': 'ts-jest',
+    '^.+\\.jsx?$': 'babel-jest',
+  },
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
+  snapshotSerisalizers: ['jest-serializer-vue'],
+  testMatch: [
+    '<rootDir>/tests/unit/**/*.spec.(js|jsx|ts|tsx)',
+    '<rootDir>/src/modules/**/*.spec.(js|jsx|ts|tsx)',
+  ],
+  transformIgnorePatterns: ['<rootDir>/node_modules/'],
+}
