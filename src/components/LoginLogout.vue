d1c6e2c5f659f28a0753b248fda1d10e5a6b6091
Anton Malofeev <arenukvern@gmail.com>
2020-07-29 10:03:52 +0300

    Add: input label

--- src/components/LoginLogout.vue
+++ src/components/LoginLogout.vue
@@ -3,9 +3,9 @@
   .card__title.--has-text-centered.--has-text-accent {{title}}
   .card__body 
     .column
-      v-input(v-model='user.username' :errors='errors.username')
-      v-input(v-model='user.email' :errors='errors.email')
-      v-input(v-model='user.password' :errors='errors.password')
+      v-input(v-model='user.username' :errors='errors.username' label='Username')
+      v-input(v-model='user.email' :errors='errors.email' label='E-mail')
+      v-input(v-model='user.password' :errors='errors.password' label='Password')
   .card__actions
     v-btn(@click='submit') {{actionBtnTitle}}
 </template>
@@ -15,6 +15,8 @@ import { StatelessComponentsRouter } from '../componentsRouter'
 import { User } from '../entities/User'
 import { AuthService } from '@/services/AuthService'
 import { Constants } from '../constants/Constants'
+import { RoutesPaths } from '../constants/RoutesPaths'
+
 const VBtn = StatelessComponentsRouter.VBtn
 const VInput = StatelessComponentsRouter.VInput
 
@@ -55,6 +57,11 @@ export default class LoginLogoutComponent extends Vue {
     const data = await resp.json()
     if (resp.ok) {
       localStorage.setItem(Constants.token, (data as User).token)
+      if (this.$route.params.nextUrl != null) {
+        this.$router.push(this.$route.params.nextUrl)
+      } else {
+        this.$router.push(RoutesPaths.home)
+      }
     } else {
       this.errors = data
     }
--- src/components/Stateless/VInput.vue
+++ src/components/Stateless/VInput.vue
@@ -1,12 +1,19 @@
 <template lang="pug">
-.input
-  input(v-model='pInput') 
+label.input(:for='uiid')
+  {{label}}
+  input(v-model='pInput' :id='uuid') 
   .input__errors {{error}}
 </template>
 <script lang="ts">
 import { Component, Prop, Vue } from 'vue-property-decorator'
+import { v4 as uuidv4 } from 'uuid'
+
 @Component({})
 export default class VInput extends Vue {
+  mounted() {
+    this.uuid = uuidv4()
+  }
+  uuid: string = ''
   @Prop({ required: true }) readonly value: string
   set pInput(value: string) {
     this.$emit('input', value)
@@ -18,5 +25,6 @@ export default class VInput extends Vue {
   get error() {
     return Array.isArray(this.errors) ? this.errors[0] : this.errors
   }
+  @Prop({ required: true }) readonly label: string
 }
 </script>
