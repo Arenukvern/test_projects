25bc189d37efd4d1aad84a629af6022db82cc61d
Anton Malofeev <arenukvern@gmail.com>
2021-04-01 23:54:47 +0300

    perf: scaffold

--- packages/auth-vue3/src/App.vue
+++ packages/auth-vue3/src/App.vue
@@ -1,6 +1,14 @@
-<template>
-  <img alt="a tree" src="./assets/tree.svg" />
-  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+<template lang="pug">
+img.absolute.b-0.l-0.z-0(src='./assets/tree.svg' alt="a tree" )
+.column.relative.z-10
+  .column.align-center
+    div Welcome
+    .row
+      input(type='text')
+      button(alt='send')
+  .row
+    button Sign in
+    button Sign up
 </template>
 
 <script lang="ts">
