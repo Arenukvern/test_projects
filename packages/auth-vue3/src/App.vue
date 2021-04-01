abfcf23ffb14e605f294b5495b5dab3b5e13fee3
Anton Malofeev <arenukvern@gmail.com>
2021-04-02 01:01:40 +0300

    feat: title, row, column

--- packages/auth-vue3/src/App.vue
+++ packages/auth-vue3/src/App.vue
@@ -1,14 +1,15 @@
 <template lang="pug">
 img.absolute.b-0.l-0.z-0(src='./assets/tree.svg' alt="a tree" )
-.column.relative.z-10
-  .column.align-center
-    div Welcome
-    .row
-      input(type='text')
-      button(alt='send')
-  .row
-    button Sign in
-    button Sign up
+.grid.columns-1.rows-90p.h-full.relative.z-10
+  .column.center
+    .column.main-align-space-between.h-6
+      p.text.center.h4 Welcome back
+      .row
+        input(type='text')
+        button.button(alt='send')
+  .row.cross-align-end
+    button.button.w-full Sign in
+    button.button.w-full Sign up
 </template>
 
 <script lang="ts">
