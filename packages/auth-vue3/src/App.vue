417483e1f1d3cba9b45fcccda3ca8950c3bd0967
Anton Malofeev <arenukvern@gmail.com>
2021-04-02 04:26:46 +0300

    feat: completed mobile

--- packages/auth-vue3/src/App.vue
+++ packages/auth-vue3/src/App.vue
@@ -5,63 +5,27 @@ img.absolute.b-0.l-0.z-0(src='./assets/tree.svg' alt="a tree" )
     .column.main-align-space-between.h-6
       p.text.center.h4 Welcome back
       .row.cross-align-center
-        input(
-            type='text'
-            class=`            
-            h-2
-            hover-back-coffee-bean-2
-
-            focus-back-coffee-bean-5
-            focus-shadow-26-coffee-bean-4
-
-            border-radius-5
-            border-bottom-1
-            border-bottom-dashed
-            border-bottom-color-seashell
-
-            pl-4
-            pr-4
-
-            back-color-seashell
-            
-            transition-duration-04
-            transition-all
-            transition-easy
-            `
-            placeholder='email'
-          )
-        button(
-          class=`
-            ml-4
-
-            button
-            icon
-            
-            transition-duration-04
-            transition-all
-            transition-easy
-            
-            hover-scale-12
-            hover-back-coffee-bean-2
-            focus-back-coffee-bean-5
-            focus-shadow-26-coffee-bean-10
-
-          `
-        )
+        field.mr-4(placeholder='email')
+        icon-button()
           img(src='./assets/send.svg')
   .row.cross-align-end
-    button.button.w-full Sign in
-    button.button.w-full Sign up
+    styled-button.border-top-right-corner-80(:isDark='true') Sign in
+    styled-button Sign up
 </template>
 
 <script lang="ts">
-  import { defineComponent } from 'vue'
-  import HelloWorld from './components/HelloWorld.vue'
+  import { defineAsyncComponent, defineComponent } from 'vue'
 
   export default defineComponent({
     name: 'App',
     components: {
-      HelloWorld,
+      Field: defineAsyncComponent(() => import('./components-core/field.vue')),
+      IconButton: defineAsyncComponent(
+        () => import('./components-core/icon-button.vue')
+      ),
+      StyledButton: defineAsyncComponent(
+        () => import('./components-core/styled-button.vue')
+      ),
     },
   })
 </script>
